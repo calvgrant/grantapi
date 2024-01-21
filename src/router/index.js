1b07400af6f261694ded90d7c74a294076bb0f8d
alvinnobeltungga@gmail.com <alvinnobeltungga@gmail.com>
2024-01-21 18:02:48 +0800

    update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const textController = require("../controller/textController.js");
-const images = require("../controller/images.js");
 const router = express.Router();
 
 router.get("/jokereceh", textController.randomTextJoke);
-
-router.get("/gay", images.gay);
 
 module.exports = router;
